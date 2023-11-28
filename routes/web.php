c85d104404338e9a9621f169dd20879974623908
tony555t <tonytullah764@gmail.com>
2023-11-28 17:58:17 +0300

    search route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,18 @@ Route::get('/', function () {
 Route::get ('post/{id}',function($id){
     return response('post'.$id);
 });
+route::delete('post/{id}',function ($id){
+    return response("delete post with id:".$id);
+});
 
+route::patch('post/{id}',function($id){
+    return response("update post with id:".$id);
+});
+route::put ('post/{$id}',function($id){
+    return response("edit post with id:".$id);
+})->where ('id','[0-9]+');
+
+route::get('/search',function(Request $request){
+    dd($request);
+
+});
