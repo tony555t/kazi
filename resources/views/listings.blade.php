a39e2dcbf137fa045b5993764040bfa5fd7a9189
tony555t <tonytullah764@gmail.com>
2023-11-30 15:52:40 +0300

    routes

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,17 @@
-<h1><?php{{$heading}}?></h1>
+{{--adding dependencies--}}
+
+<h1>{{$heading}}</h1>
+@if (count ($listings)==0)
+@endif
+<p> no listings found</p>
+{{-- looping through each listing in the listing array--}}
+
 @foreach ($listings as $listing)
-<h2><?php  $listing['title'];?></h2>
-<p><?php echo $listing['description'];?></p>
+<h2>
+    {{$listing['title']}}
+</h2>
+<p>
+     {{$listing['description']}}
+</p>
 
 @endforeach
